6fea97e8b1663ed131c2e0170f51563e61596656
nicoleyip1876 <17805096+nicoleyip1876@users.noreply.github.com>
2020-04-26 00:53:54 +0000

    Updating all questions

--- invent-code-0/main.cpp
+++ invent-code-0/main.cpp
@@ -7,6 +7,7 @@ int main() {
   cout << "Enter how many numbers of the Fibonacci sequence you would like to print out: ";
   cin >> length;
 
+  // special case if length is 3 or less
   if (length == 1) {
     cout << "0";
   } else if (length == 2) {
@@ -14,20 +15,17 @@ int main() {
   } else if (length == 3) {
     cout << "0 1 1";
   }
-  // Write code here.
 
+  // if length is 3 or more.. 
   if (length > 3) {
     cout << "0 1 1";
-  
-  int result = 1;
-  int previous_result = 1;
-  for (int i = 2; i <= (length - 2); i++) {
-    
-    result = result + previous_result;
-    previous_result = result - previous_result;
-    cout << " " << result;
-  }
-  // Hint check for special cases when length < 2
+    int result = 1;
+    int previous_result = 1;
+    for (int i = 2; i <= (length - 2); i++) { 
+      result = result + previous_result;
+      previous_result = result - previous_result;
+      cout << " " << result;
+    }
   }
   return 0;
 }
