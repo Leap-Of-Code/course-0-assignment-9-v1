0a31aeb54555e0168a1a732595e009d9d8dc2171
nicoleyip1876 <17805096+nicoleyip1876@users.noreply.github.com>
2020-04-26 01:00:54 +0000

    Updating all questions

--- fix-code-2/main.cpp
+++ fix-code-2/main.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main() {
+  int counter = 0;
   for (int i = 0; i < 12; i++) {
     cout << "Learning to code is fun.";
+    counter++;
   }
-  cout << "I used loops to print out (Learning code is fun) " << i << " times.";
+  cout << "I used loops to print out (Learning code is fun) " << counter << " times." << endl;
   return 0;
 }
