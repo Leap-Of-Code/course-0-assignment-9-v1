eb930e3e240317ae875fd4d90d4fb831f10ac41c
vivianpppp <63162644+vivianpppp@users.noreply.github.com>
2020-05-07 23:44:40 +0000

    Saving progress for all questions.

--- fix-code-3/main.cpp
+++ fix-code-3/main.cpp
@@ -4,9 +4,8 @@ using namespace std;
 
 int main() {
   // This code prints out all the even numbers from 0 to 100 inclusively.
-  for (int counter = 0; counter < 100; counter++) {
+  for (int counter = 0; counter <= 100; counter = counter + 2) {
     cout << "Counter: " << counter << endl;
-    counter = 50;
   }
   return 0;
 }
--- fix-code-4/main.cpp
+++ fix-code-4/main.cpp
@@ -9,14 +9,19 @@ int SumOfEvenNumbers(int low, int high) {
         sum += i;
         return sum;
     }
+    return sum;
 }
 
 int main() {
-    int bottom = 10; // Make this a user input.
-    int top = 18;    // Make this a user input.
+    int bottom; //= 10; // Make this a user input.
+    int top; // = 18;    // Make this a user input.
+    cout << "Enter a number: ";
+    cin >> bottom;
+    cout << "Enter another number: ";
+    cin >> top;
 
     // for 10 and 18, this should return 52.
-    int even_sum = SumofEvenNumbers(bottom, top);
+    int even_sum = SumOfEvenNumbers(bottom, top);
 
     // Do update this section based on what numbers are entered by the user.
     cout << "10 + 12 + 14 + 16" << " = ";
