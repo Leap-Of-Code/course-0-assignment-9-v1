8bde16c82841385f8029874e7c5643f27865c126
cyndisong28 <30833889+cyndisong28@users.noreply.github.com>
2020-05-08 03:56:11 +0000

    Saving progress for all questions.

--- invent-code-0/main.cpp
+++ invent-code-0/main.cpp
@@ -2,11 +2,23 @@
 #include <string>
 using namespace std;
 
+void PrintMagicNumber (unsigned int length) {
+  if (length == 0) {
+    cout << length;
+  }
+  if (length == 1 || length == 2)
+    cout << "1";
+  for (unsigned int output = 0; output <= length; output += output ){
+    cout << output << " ";
+  }
+}
+
+
 int main() {
   unsigned int length;
   cout << "Enter how many numbers of the Fibonacci sequence you would like to print out: ";
   cin >> length;
-
+  PrintMagicNumber(length);
   // Write code here.
   // Hint check for special cases when length < 2
   return 0;
